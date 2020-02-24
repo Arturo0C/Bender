0e4fbfb0613e46418bbfe14a79033922b36be045
Arturo0C <arturobuendia3@gmail.com>
2020-02-24 17:49:47 +0100

    tengo que cambiar las arrays por arrays irregulares en cuanto termine los mapas comenzare con el resto.

--- src/main.java
+++ src/main.java
@@ -7,28 +7,30 @@ public class main {
     /// Descomponiendo el mapa
     public static void main(String[] args) {
         String mapa = "" +
-                "#######\n" +
-                "#     #\n" +
-                "#     #\n" +
-                "#    $#\n" +
-                "#     #\n" +
-                "# X   #\n" +
-                "#     #\n" +
-                "#######";
+                "   #######\n" +
+                "   # XTI #\n" +
+                "   #    $#\n" +
+                "####    #####\n" +
+                "#          T#\n" +
+                "####     ####\n" +
+                "   #    I#\n" +
+                "   #     #\n" +
+                "   #######";
 
-        String[] size = new String[mapa.length()];
 
+        // Calculamos la longitud de el mapa
         int cont = 0;
-        int horizontal2 = 0;
-        int vertical2 = 1;
+        int horizontal = 0;
+        int vertical = 1;
+
 
         for (int i = 0; i < mapa.length(); i++) {
 
-            if (mapa.charAt(i) == '\n'){
-                vertical2++;
+            if (mapa.charAt(i) == '\n') {
+                vertical++;
 
-                if(horizontal2 <= cont){
-                    horizontal2 = cont;
+                if (horizontal <= cont) {
+                    horizontal = cont;
                     cont = 0;
                     continue;
                 }
@@ -36,40 +38,29 @@ public class main {
             cont++;
         }
 
-        out.println(horizontal2);
-        out.println(vertical2);
-
-
-        Bender b = new Bender(mapa);
-
-        int horizontal = 0;
-        int vertical = 0;
-
-        for (int i = 0; i < mapa.length(); i++) {
-            if (horizontal == 0) {vertical ++;}
-            if (mapa.charAt(i) == '\n') {horizontal++;}
-        }
+        out.println(vertical);
+        out.println(horizontal);
+        out.println(mapa.length());
 
         Character[][] mapa2d = new Character[vertical][horizontal];
         Integer[] posicionPersonaje = new Integer[2];
 
+        /*
         // Variable para contar el caracter de mapa
-        int k = 0;
+        int k = 0; //no pude superar el tamaño de el mapa
         //v
         for (int i = 0; i < mapa2d.length; i++) {
             //h
             for (int j = 0; j < mapa2d[0].length; j++) {
-                if (mapa.charAt(k) == '\n') {k++;}
                 mapa2d[i][j] = mapa.charAt(k);
-
+                k++;
                 // Capturamos el personaje
                 if (mapa.charAt(k) == 'X' || mapa.charAt(k) == 'x') {
                     posicionPersonaje[0] = j;
                     posicionPersonaje[1] = i;
                 }
-                k++;
             }
-        }
+        }*/
 
         for (int i = 0; i < mapa2d.length; i++) {
             for (int j = 0; j < mapa2d[0].length; j++) {
