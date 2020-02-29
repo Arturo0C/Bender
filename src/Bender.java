7ef4a56ab632f4cd342caa2d5c3829040614d60c
Arturo0C <arturobuendia3@gmail.com>
2020-02-29 23:30:42 +0100

    Teleporter creado, pensando como implementarlo

--- src/Bender.java
+++ src/Bender.java
@@ -1,22 +1,21 @@
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 class Bender {
     // Constructor: ens passen el mapa en forma d'String
 
-    char[][] mapa2d;
-    Robot robot = new Robot();
-
-    //Posicion de X
+    private char[][] mapa2d;
+    private Robot robot = new Robot();
+    private Map<Integer,Teleporter> listaTp = new HashMap<>();
 
     public Bender(String mapa) {
-        //longitud del mapa
-        int cont = 0;
+
         int horizontal = 0;
         int vertical = 1;
+        //longitud del mapa
+        int cont = 0;
 
+        //Longitud del array
         for (int i = 0; i < mapa.length(); i++) {
 
             if (mapa.charAt(i) == '\n') {
@@ -33,55 +32,51 @@ class Bender {
 
         this.mapa2d = new char[vertical][horizontal];
 
-        // Variable para contar el caracter de mapa
-        int numLletra = 0; //no pude superar el tamaño de el mapa
-        //v
+        int numLletra = 0;
+        int contadorTp = 1;
+
         for (int i = 0; i < vertical; i++) {
-            //h
             for (int j = 0; j < horizontal + 1; j++, numLletra++) {
-                if (mapa.length() == numLletra) {
-                    break;
-                }
-                if (mapa.charAt(numLletra) == '\n' && j != 0) {
-                    numLletra++;
-                    break;
-                }
+                if (mapa.length() == numLletra) { break; }
+                if (mapa.charAt(numLletra) == '\n' && j != 0) { numLletra++; break; }
 
                 mapa2d[i][j] = mapa.charAt(numLletra);
 
-                // Capturamos el personaje
+                // Capturamos el robot
                 if (mapa.charAt(numLletra) == 'X' || mapa.charAt(numLletra) == 'x') {
                     robot.setVertical(i);
                     robot.setHorizontal(j);
                 }
-
+                // Capturamos el teleporter
+                if (mapa.charAt(numLletra) == 'T' || mapa.charAt(numLletra) == 't') {
+                    listaTp.put(contadorTp, new Teleporter(i,j));
+                    robot.setListaTp(listaTp);
+                }
             }
-            if (mapa.length() == numLletra) {
-                break;
-            }
+            if (mapa.length() == numLletra) { break; }
         }
 
-
     }
-
-    // Navegar fins a l'objectiu («$»).
-
-    // El valor retornat pel mètode consisteix en una cadena de
-    // caràcters on cada lletra pot tenir
-    // els valors «S», «N», «W» o «E»,
-    // segons la posició del robot a cada moment.
 
     public String run() {
         return robot.getMovimientos(mapa2d);
     }
+
+
 }
 
 // S E N O
 class Robot {
+
     private int vertical;
     private int horizontal;
+    private Map<Integer,Teleporter> listaTp;
 
     StringBuilder movimientos = new StringBuilder();
+
+    public void setListaTp(Map<Integer, Teleporter> listaTp) {
+        this.listaTp = listaTp;
+    }
 
     public String getMovimientos(char[][] mapa2d) {
         move(vertical, horizontal, mapa2d);
@@ -110,6 +105,9 @@ class Robot {
                     if (mapa2d[v + 1][h] == ' ' || mapa2d[v + 1][h] == '$') {
                         movimientos.append("S");
                         v++;
+                    }
+                    if (mapa2d[v + 1][h] == 'T') {
+                        
                     }
                     break;
                 case 2:
@@ -149,18 +147,13 @@ class Teleporter{
     private int vertical;
     private int horizontal;
 
-    Teleporter() {}
-
-    public void setVertical(int vertical) {
+    Teleporter(int vertical, int horizontal) {
         this.vertical = vertical;
+        this.horizontal = horizontal;
     }
 
     public int getVertical() {
         return vertical;
-    }
-
-    public void setHorizontal(int horizontal) {
-        this.horizontal = horizontal;
     }
 
     public int getHorizontal() {
